1fc3e2ca550001b4cadc76422c0efc9169a60993
jamiemoram <jamiemoram@hotmail.com>
2020-07-05 02:17:39 +0100

    hello world

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Seb & Tom are GAY together'),
+      ),
+    ),
+  );
+}
